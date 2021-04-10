d0d8a525d250877e20ce2aec557579845b252af0
SaummayS <saummay7@gmail.com>
2021-04-10 18:39:47 -0500

    Set up

--- projects.js
+++ projects.js
@@ -1,12 +1,14 @@
 let projects = [
 
-{"name": "Cool Project 1",
+{"display":true,
+"name": "Cool Project 1",
 "tech": "HTML,CSS,JS,RWD",
 "url_to_page": "",
 "url_to_repo": "",
 "date": "Spring 2021"},
 
-{"name": "Cool Project 2",
+{"display":true,
+"name": "Cool Project 2",
 "tech": "HTML,CSS,JS,RWD",
 "url_to_page": "",
 "url_to_repo": "",
