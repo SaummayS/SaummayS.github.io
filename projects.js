db4dee1ba4e6afae49b3691a723ae3156fd2e86f
SaummayS <saummay7@gmail.com>
2021-04-10 20:27:06 -0500

    Set up 2

--- projects.js
+++ projects.js
@@ -1,18 +1,18 @@
 let projects = [
 
     {"display":true,
-    "name": "Cool Project 1",
+    "name": "Speech Synthesis",
     "tech": "HTML,CSS,JS,RWD",
-    "desc": "Something about it",
+    "desc": "providing user a method to convert text to speech",
     "img": "images/1.png",
     "url_to_page": "https://saummays.github.io/IT202-Spring2021-activities/lab10.html",
     "url_to_repo": "https://github.com/SaummayS/IT202-Spring2021-activities",
     "date": "Spring 2021"},
     
     {"display":true,
-    "name": "Cool Project 2",
-    "tech": "HTML,CSS",
-    "desc": "Something about it",
+    "name": "Location Aware Map",
+    "tech": "HTML,CSS,JS",
+    "desc": "Using google maps geolocation api to display live location",
     "img": "images/2.png",
     "url_to_page": "https://saummays.github.io/IT202-Spring2021-activities/locationAwareMap.html",
     "url_to_repo": "https://github.com/SaummayS/IT202-Spring2021-activities",
