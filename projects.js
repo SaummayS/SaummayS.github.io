ac15f62aa9ab95298457fde4d48bbfc84020fbef
SaummayS <saummay7@gmail.com>
2021-04-10 20:21:37 -0500

    Set up

--- projects.js
+++ projects.js
@@ -1,17 +1,21 @@
 let projects = [
 
-{"display":true,
-"name": "Cool Project 1",
-"tech": "HTML,CSS,JS,RWD",
-"url_to_page": "",
-"url_to_repo": "",
-"date": "Spring 2021"},
-
-{"display":true,
-"name": "Cool Project 2",
-"tech": "HTML,CSS,JS,RWD",
-"url_to_page": "",
-"url_to_repo": "",
-"date": "Spring 2021"}
-
-];
+    {"display":true,
+    "name": "Cool Project 1",
+    "tech": "HTML,CSS,JS,RWD",
+    "desc": "Something about it",
+    "img": "images/1.png",
+    "url_to_page": "https://saummays.github.io/IT202-Spring2021-activities/lab10.html",
+    "url_to_repo": "https://github.com/SaummayS/IT202-Spring2021-activities",
+    "date": "Spring 2021"},
+    
+    {"display":true,
+    "name": "Cool Project 2",
+    "tech": "HTML,CSS",
+    "desc": "Something about it",
+    "img": "images/2.png",
+    "url_to_page": "https://saummays.github.io/IT202-Spring2021-activities/locationAwareMap.html",
+    "url_to_repo": "https://github.com/SaummayS/IT202-Spring2021-activities",
+    "date": "Spring 2021"}
+    
+    ];
